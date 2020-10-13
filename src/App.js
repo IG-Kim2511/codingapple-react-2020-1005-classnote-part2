f275f77c37f1cc3e8cec2adffcb7ba65155a3be8
IG-Kim <iikim2511@gmail.com>
2020-10-13 15:14:15 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+// 15 쇼핑몰 프로젝트 : 프로젝트 생성 & Bootstrap 설치
+
+function App15() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    <div className="black-nav"> 15 쇼핑몰 프로젝트 : 프로젝트 생성 & Bootstrap 설치 </div>
+   </div>
   );
+}
+
+function App(){
+  return(
+    <div>
+       <App15/>      
+    </div>
+  )
 }
 
 export default App;
