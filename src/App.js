76597697b35eb043195725765b11aa50b0367505
IG-Kim <iikim2511@gmail.com>
2020-10-13 18:01:54 -0400

    16 React Bootstrap. 평화로운 쇼핑몰 레이아웃 디자인

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+// 16-(1)
+import { Navbar , Nav ,NavDropdown , Form , FormControl, Button, Jumbotron} from 'react-bootstrap';
+
+
 
 // 15 쇼핑몰 프로젝트 : 프로젝트 생성 & Bootstrap 설치
 //(1)yarn
@@ -30,10 +34,88 @@ function App15() {
   );
 }
 
+// 16 React Bootstrap. 평화로운 쇼핑몰 레이아웃 디자인
+// (1) React Bootstrap 홈페이지에서 Import Components 복사-붙여넣기
+// (1-2) 사용할 component이름 (Navbar , Nav ,NavDropdown , Form , FormControl, Button) 모두 목록에 추가
+// (1-3) 대문자 시작 태그들 모두가 Component
+
+// (2) css className 추가 가능
+
+// bootstrap 검색어 : jumbotron, layout
+
+function App16() {
+  return (
+   <div className="App">
+    <div className="black-nav">16 React Bootstrap. 평화로운 쇼핑몰 레이아웃 디자인  </div>
+
+    <div>
+    // (2)navbar
+    <Navbar bg="light" expand="lg" className='background'>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        
+      </Navbar.Collapse>
+
+              // Jumbotron
+     <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      // 상품 레이아웃
+      <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div>
+      </div>
+      </div>
+
+      </Navbar>
+    </div>
+
+
+
+
+   </div>
+  );
+}
+
 function App(){
   return(
     <div>
        <App15/>      
+       <App16/>      
     </div>
   )
 }
