d716a5137db0446e456add8b462afdc60b9cf6b9
IG-Kim <iikim2511@gmail.com>
2020-11-05 18:22:46 -0500

    🦄40 PWA : Progressive Web App 발행하기

--- src/App.js
+++ src/App.js
@@ -1493,6 +1493,39 @@ function App35() {
 
 // }
 
+// 🦄40 PWA : Progressive Web App 발행하기
+// → 캡쳐본 확인
+
+// (1)
+// index.js에서 serviceWorker.register();
+// npm run build
+
+// (2)
+// → build/manifest.json
+// → build/precache-manifest.js
+// → build/service-worker.js
+
+// (3)
+// → node_modules/react-scripts/config/webpack.config.js 
+// 나의 PWA를 커스터마이징하려면
+// 지금 PWA 발행이 쉽고 간단한 이유는 구글의 workbox 라는 라이브러리 덕분입니다.
+// 이게 create-react-app 설치할 때 함께 설치되었기 때문입니다.
+// 그래서 PWA 발행방식 같은걸 커스터마이징 하고싶으면 workbox 사용법을 익히셔야하는데
+// 구글 직원들이 써놓은 개발문서 같은거 보면 매우 불친절하고 어렵습니다. 
+
+// 그래서 빠르게 커스터마이징 방법 하나만 알려드리겠습니다. 
+// Q. 하드에 설치할 파일 중에 HTML을 제외하고 싶다면?
+// 이런 경우 많습니다. HTML 파일은 너무 자주 변해서 하드에 저장해놓기 싫다면 여길 수정하시면 됩니다.
+
+// 여러분 프로젝트 폴더 내의 
+// node_modules/react-scripts/config/webpack.config.js 파일을 찾으시면 됩니다. 
+// 거기 하단 쯤에 보면 이런 코드가 있습니다. 
+// "new WorkboxWebpackPlugin.GenerateSW({ 어쩌구~   "
+
+// build 할 때 index.html 파일을 캐싱목록에서 제외해주게 됩니다.
+// 오늘도 service-worker 쉽게 만들어주는 구글신님께 감사인사를 올리도록 합시다. 
+// (근데 여러분 사이트가 페이스북, 인스타, 유튜브처럼 입장과 동시에 Ajax로 초기데이터들을 전부 받아오는 사이트라면 굳이 HTML 파일을 저렇게 할 필요는 없겠죠?)
+ 
 
 function App(){
   return(
